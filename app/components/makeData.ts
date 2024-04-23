72901b2098934d885b51239151dd28662b98ff83
Karolina Jimenez G <karojg@gmail.com>
2024-04-22 20:34:14 -0600

    Initial Table

---
+++ app/components/makeData.ts
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker";
+
+export type Person = {
+  id: number;
+  product: string;
+  type: string;
+  size: string;
+  unit: string;
+  price: number;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/Navbar";
+import { TableComponent } from "./components/Table";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between w-full">
       <Navbar />
+      <TableComponent />
     </main>
   );
 }
